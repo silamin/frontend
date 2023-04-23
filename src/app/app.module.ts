940f002ca801faa5a62db607e8c5f16bdf81e029
Danylo <91031103+silamin@users.noreply.github.com>
2023-04-23 16:24:29 +0200

    sign in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import {RouterOutlet} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent
   ],
-    imports: [
-        BrowserModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterOutlet
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss']
+})
+export class SignInComponent {
+  userlogin = true;
+  userregister = false;
+  //Buttons clicks functionalities
+  user_register()
+  {
+    this.userlogin = false;
+    this.userregister = true;
+  }
+  user_login()
+  {
+    this.userlogin = true;
+    this.userregister = false;
+  }
+
+}
